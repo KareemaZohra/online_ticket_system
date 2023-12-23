bbd748177e703029d0ba21205e42cd5c98f873b5
Kareema Zohra <KareemaZohra@users.noreply.github.com>
2023-12-23 22:22:24 +0600

    design seats of bus and check if seat already reserved

--- app/Http/Controllers/tripController.php
+++ app/Http/Controllers/tripController.php
@@ -13,26 +13,40 @@ class tripController extends Controller
 
     public function reservation(){
         $locations = location::all();
+        $reserved_seats = seat_allocation::pluck('seat_no');
 
-        return view('bookTrip', compact('locations'));
+        return view('bookTrip', compact('locations','reserved_seats'));
     }
 
     public function booking(Request $request){
         $input = $request->all();
 
-        $trip = new trip;
-        $trip->location = $input['location'];
-        $trip->trip_date = $input['trip_date'];
-        $trip->save();
+        $seat_no = $input['seat_no'];
+        $checkIfExists = trip::whereHas('seats', function ($q) use ($seat_no){
+            $q->where('seat_allocation.seat_no',$seat_no);
+        })
+            ->where('trip_date',$input['trip_date'])
+            ->where('location',$input['location'])
+            ->get();
 
-        $latest_trip = trip::latest()->first();
+        if(count($checkIfExists)>0){
+            return "This seat is already reserved";
+        }
+        else{
+            $trip = new trip;
+            $trip->location = $input['location'];
+            $trip->trip_date = $input['trip_date'];
+            $trip->save();
 
-        $seat = new seat_allocation;
-        $seat->seat_no = $input['seat_no'];
-        $seat->trip_id = $latest_trip->id;
-        $seat->save();
+            $latest_trip = trip::latest()->first();
 
-        return redirect()->back();
+            $seat = new seat_allocation;
+            $seat->seat_no = $input['seat_no'];
+            $seat->trip_id = $latest_trip->id;
+            $seat->save();
+
+            return redirect()->back();
+        }
 
     }
 }
--- resources/views/bookTrip.blade.php
+++ resources/views/bookTrip.blade.php
@@ -26,10 +26,21 @@
             <div class="col-md-6">
                 <div class="container">
                     <div class="row">
+                        <div class="col-md-6">Available seats Color : Blue</div>
+                        <div class="col-md-6">Reserved seats color : Gray</div>
+                    </div>
+                    <br>
+                    <div class="row">
                         @for($i = 1; $i<= 36; $i++)
-                        <div class="col-2">
-                            <div class="bus text-center">{{$i}}</div>
-                        </div>
+                            @if(in_array($i, $reserved_seats->toArray()))
+                                <div class="col-2">
+                                    <div class="bus bus-reserved text-center">{{$i}}</div>
+                                </div>
+                            @else
+                                <div class="col-2">
+                                    <div class="bus text-center">{{$i}}</div>
+                                </div>
+                            @endif
                         @endfor
                     </div>
                 </div>
