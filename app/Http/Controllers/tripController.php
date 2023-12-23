ca5e4592bfc841661b8c0e1bc80acddf6a063ec4
Kareema Zohra <KareemaZohra@users.noreply.github.com>
2023-12-23 20:42:55 +0600

    unwanted views removed and reservation view created

--- app/Http/Controllers/tripController.php
+++ app/Http/Controllers/tripController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\location;
 
 class tripController extends Controller
 {
     //
+
+    public function reservation(){
+        $locations = location::all();
+
+        return view('bookTrip', compact('locations'));
+    }
 }
